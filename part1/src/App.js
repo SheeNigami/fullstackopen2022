e6f37eb73d015f16d3fd4301453307fd71d97766
SheeNigami <sheenhern@outlook.com>
2022-06-02 21:31:52 +0800

    React Dynamic Components

--- part1/src/App.js
+++ part1/src/App.js
@@ -2,9 +2,16 @@
 // import './App.css';
 
 function App() {
+  const now = new Date()
+  const a = 10
+  const b = 20
+
   return (
     <div>
-      <p>Hello world</p>
+      <p>Hello world, it is now {now.toString()}</p>
+      <p>
+        {a} plus {b} is {a+b}
+      </p>
     </div>
   );
 }
