bb9fddea185ff4c8c0db0865e15f9cecef795d41
SheeNigami <sheenhern@outlook.com>
2022-06-02 23:31:04 +0800

    Age guesser feature added

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,29 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
-function App() {
+const App = () => {
   const name = 'Peter'
   const age = 10
 
   return (
-    <>
+    <div>
       <h1>Greetings</h1>
-      <Hello name="George" age={26+10}/>
-      <Hello name={name} age={age}/>
-    </>
-  );
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
 }
 
 export default App;
