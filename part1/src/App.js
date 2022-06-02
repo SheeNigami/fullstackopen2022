2dcd5288e573045201643b12b2ba6b5ed2707f5e
SheeNigami <sheenhern@outlook.com>
2022-06-03 00:14:22 +0800

    Left/Right and History functionality added

--- part1/src/App.js
+++ part1/src/App.js
@@ -3,34 +3,49 @@ import { useState } from 'react'
 const Display = ({ counter }) => <div>{counter}</div>
 
 
-const Button = ({ onClick, text }) => (
-  <button onClick={onClick}>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
     {text}
   </button>
 )
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-  const increaseByOne = () => setCounter(counter + 1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
 
   return (
     <div>
-      <Display counter={counter}/>
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
